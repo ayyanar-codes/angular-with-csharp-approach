c21f894e4e93da08bc08b4242927bc2362ff07c8
Ayyanar <48320634+ayyanar-codes@users.noreply.github.com>
2024-08-29 17:00:50 +0530

    Add files via upload

---
+++ 4.HTML-element-data-binding-local-reference/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  
+  title = '4.HTML-element-data-binding-local-reference';
+
+ 
+
+
+}
